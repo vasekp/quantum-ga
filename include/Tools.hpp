b80e221ea125f9239ac1432c02e45a365aba6ea7
Vašek Potoček <vasek.potocek@post.cz>
2016-10-14 12:19:46 +0100

    Drop bernoulli_distribution_s in favour of std::bernoulli_distribution
    RNG calls are actually cheaper than reusing the previous value.

--- include/Tools.hpp
+++ include/Tools.hpp
@@ -49,63 +49,9 @@ enum class Controls {
   ANY
 };
 
-/* A helper class for controls_distribution */
-
-class bernoulli_distribution_s {
-
-  const double pTrue;
-
-  // entropy decrease per true result
-  const double dSTrue;
-  // entropy decrease per false result
-  const double dSFalse;
-  // entropy minimum left for reliable generation
-  const double Smin;
-
-  // random value uniformly distributed between 0 and 1
-  double c;
-  // entropy available in c
-  double S;
-
-public:
-
-  bernoulli_distribution_s(double pTrue_):
-    pTrue(pTrue_),
-    dSTrue(-std::log(pTrue) / std::log(2.0)),
-    dSFalse(-std::log(1 - pTrue) / std::log(2.0)),
-    Smin(std::min(std::max(dSTrue, dSFalse), (double)S0())),
-    c(0), S(0) { }
-
-  // entropy in a fully randomized double
-  // can't be a normal static constexpr member due to language limitations
-  static constexpr size_t S0() {
-    return std::numeric_limits<double>::digits;
-  }
-
-  template<class URNG>
-  bool operator() (URNG& rng) {
-    if(S < Smin) {
-      c = std::generate_canonical<double, S0()>(rng);
-      S = S0();
-    }
-    if(c < pTrue) {
-      c /= pTrue;
-      S -= dSTrue;
-      return true;
-    } else {
-      c = (c - pTrue)/(1 - pTrue);
-      S -= dSFalse;
-      return false;
-    }
-  }
-
-}; // class bernoulli_distribution_s
-
-
 /* A distribution generating bit strings of length nBit where the probability
  * of 1 in each position is given by pTrue. The bit at position nSkip is left
- * off. The provided uniform RNG is invoked only the minimum necessary number
- * of times. */
+ * off. */
 
 template<Controls cc>
 class controls_distribution {
@@ -129,7 +75,7 @@ public:
     std::vector<bool> bits(nBit, false);
 
     if(cc == Controls::ANY || cc == Controls::LEAST1) {
-      bernoulli_distribution_s dist(pTrue);
+      std::bernoulli_distribution dist(pTrue);
       for(unsigned i = 0; i < nBit; i++) {
         if(i == iSkip)
           continue;
