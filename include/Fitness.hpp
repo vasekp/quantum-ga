61c0e92367274d002ae5c5c25f20832887aa411a
Vašek Potoček <vasek.potocek@post.cz>
2016-10-18 18:23:37 +0100

    Include Counter.hpp into Fitness.hpp

--- include/Fitness.hpp
+++ include/Fitness.hpp
@@ -33,4 +33,85 @@ struct Fitness {
 
 }; // struct Fitness
 
+
+namespace internal {
+
+/* Specialized as Counter<A, B, C, ...>, this template holds one counter per
+ * each type given in the parameter pack. It defines member functions
+ * hit(const A*), hit(const B*), ... which each bump their respective counter.
+ * There are also dominance comparison operators and a stream operator<<
+ * needed for a seamless inclusion in Fitness. */
+
+template<class Head, class... Tail>
+class Counter: public Counter<Tail...> {
+
+  unsigned cnt = 0;
+
+public:
+
+  Counter() = default;
+
+  void hit(const Head*) {
+    cnt++;
+  }
+
+  using Counter<Tail...>::hit;
+
+  friend std::ostream& operator<< (std::ostream& os, const Counter& c) {
+    return os << c.cnt << ',' << c.next();
+  }
+
+  friend bool operator<<= (const Counter& c1, const Counter& c2) {
+    return c1.cnt <= c2.cnt && (c1.next() <<= c2.next());
+  }
+
+  friend bool operator== (const Counter& c1, const Counter& c2) {
+    return c1.cnt == c2.cnt && c1.next() == c2.next();
+  }
+
+  friend bool operator<< (const Counter& c1, const Counter& c2) {
+    return c1 <<= c2 && !(c1 == c2);
+  }
+
+private:
+
+  const Counter<Tail...>& next() const {
+    return static_cast<const Counter<Tail...>&>(*this);
+  }
+
+}; // class Counter<Head, Tail...>
+
+template<class Last>
+class Counter<Last> {
+
+  unsigned cnt = 0;
+
+public:
+
+  Counter() = default;
+
+  void hit(const Last*) {
+    cnt++;
+  }
+
+  friend std::ostream& operator<< (std::ostream& os, const Counter& c) {
+    return os << c.cnt;
+  }
+
+  friend bool operator<<= (const Counter& c1, const Counter& c2) {
+    return c1.cnt <= c2.cnt;
+  }
+
+  friend bool operator== (const Counter& c1, const Counter& c2) {
+    return c1.cnt == c2.cnt;
+  }
+
+  friend bool operator<< (const Counter& c1, const Counter& c2) {
+    return c1 < c2;
+  }
+
+}; // class Counter<Last>
+
+} // namespace internal
+
 } // namespace QGA
