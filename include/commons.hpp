e02436e29efe142ee24d8e3165b50a7f85608406
Vašek Potoček <vasek.potocek@post.cz>
2016-10-18 15:18:56 +0100

    Reorganized #includes, with explanatory comments

--- include/commons.hpp
+++ include/commons.hpp
@@ -56,15 +56,14 @@ namespace QGA {
 #endif
 
 #include "GateBase.hpp"
-#include "Gene.hpp"
+#include "Gene.hpp"     // uses GateBase.hpp
+#include "Tools.hpp"
+#include "Gates.hpp"    // uses Tools.hpp and GateBase.hpp
 #include "Fitness.hpp"
 #include "CandidateCounter.hpp"
 #include "CandidateBase.hpp"
 #include "CandidateFactory.hpp"
 
 #include "Colours.hpp"
-
-#include "Tools.hpp"
-#include "Gates.hpp"
 
 #endif // !defined QGA_COMMONS_HPP
