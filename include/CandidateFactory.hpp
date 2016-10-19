38aa245a2721d4e409237c8c8b0326673f8cd9e5
Vašek Potoček <vasek.potocek@post.cz>
2016-10-19 15:38:02 +0100

    Fixed uniform crossover
    The prior version was actually doing uniform interleaving instead (using *all*
    genes from both candidates and resulting in sz1 + sz2 while this should be
    *alternating* genes from both and produce a new genotype typically no longer
    than either).

--- include/CandidateFactory.hpp
+++ include/CandidateFactory.hpp
@@ -265,37 +265,51 @@ private:
   Candidate crossoverUniform() {
     auto &parent1 = get(),
          &parent2 = get();
-    auto &gt1 = parent1.genotype(),
-         &gt2 = parent2.genotype();
-    size_t sz1 = gt1.size(),
-           sz2 = gt2.size(),
-           szShorter = std::min(sz1, sz2);
-    double pTest1 = (double)szShorter / sz1,
-           pTest2 = (double)szShorter / sz2;
+    auto *gt1 = &parent1.genotype(),
+         *gt2 = &parent2.genotype();
+    // Most of these are here just for clarity and will hopefully be
+    // optimized away
+    size_t sz1 = gt1->size(),
+           sz2 = gt2->size(),
+           szShorter = std::min(sz1, sz2),
+           szLonger = std::max(sz1, sz2),
+           pos1 = 0, pos2 = 0;
+    // expLen will be = 1 for the shorter candidate and > 1 for the longer one
+    // (1 for both if equal length). This is the expected length of a slice we
+    // take or skip before making a new decision whether to cross over.
+    double expLen1 = (double)sz1 / szShorter,
+           expLen2 = (double)sz2 / szShorter;
     std::uniform_real_distribution<> dUni{0, 1};
+    std::geometric_distribution<size_t> dGeom1{1.0 / expLen1};
+    std::geometric_distribution<size_t> dGeom2{1.0 / expLen2};
     std::vector<Gene> gtNew{};
-    gtNew.reserve(sz1 + sz2 - szShorter);
-    auto it1 = gt1.begin(),
-         it2 = gt2.begin(),
-         ie1 = gt1.end(),
-         ie2 = gt2.end();
-    while(it1 != ie1) {
-      gtNew.push_back(*it1++);
-      if(dUni(gen::rng) < pTest1) {
-        // Whether to make this a possible crossover point.
-        // The above check passes every time on the shorter genotype,
-        // 1/ratio of the time on the longer one.
-        // We might still decide to stay on the same branch, though.
-        if(dUni(gen::rng) < Config::pCrossUniform) {
-          std::swap(it1, it2);
-          std::swap(ie1, ie2);
-          std::swap(pTest1, pTest2);
-        }
+
+    gtNew.reserve(szLonger);
+    for(;;) {
+      // Take roughly expLen1 genes from gt1
+      size_t upto = pos1 + dGeom1(gen::rng) + 1;
+      if(upto >= sz1)
+        break; // just use the rest of gt1
+      // Skip roughly expLen2 genes from gt2
+      pos2 += dGeom2(gen::rng) + 1;
+      if(pos2 >= sz2)
+        break; // ditto
+      gtNew.insert(gtNew.end(), gt1->begin() + pos1, gt1->begin() + upto);
+      pos1 = upto;
+      // With probability pCrossUniform we swap the two sources (along with
+      // all associated data), otherwise we repeat.
+      if(dUni(gen::rng) < Config::pCrossUniform) {
+        std::swap(gt1, gt2);
+        std::swap(sz1, sz2);
+        std::swap(pos1, pos2);
+        std::swap(dGeom1, dGeom2);
       }
     }
-    // now it1 is at its boundary, if there's anything left past it2 we
-    // flush it with no more crossovers
-    gtNew.insert(gtNew.end(), it2, ie2);
+    // If we get here then either more was requested of gt1 than available or
+    // gt2 went empty. In either case, we just take whatever's left and we
+    // finish the crossover operation.
+    gtNew.insert(gtNew.end(), gt1->begin() + pos1, gt1->end());
+
     return Candidate{std::move(gtNew)};
   }
 
