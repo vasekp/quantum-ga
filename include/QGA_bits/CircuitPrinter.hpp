5252beabf710c9cb86028dda779df615aa9f3ee2
Vašek Potoček <vasek.potocek@post.cz>
2016-11-07 12:43:38 +0000

    Explain the functions in CircuitPrinter

--- include/QGA_bits/CircuitPrinter.hpp
+++ include/QGA_bits/CircuitPrinter.hpp
@@ -6,15 +6,22 @@ class CircuitPrinter {
 
 public:
 
-  CircuitPrinter(unsigned nBit_): nBit(nBit_), lines(2*nBit - 1) {
+  CircuitPrinter(unsigned nBit_): nBit(nBit_), lines(2*(nBit - 1)) {
+    // lines[0], lines[2], ..., lines[2*(nBit - 1)]: qubits
+    // lines[1], lines[3], ..., lines[2*nBit - 3]: padding space
     for(unsigned i = 0; i < nBit; i++)
       lines[2*i] = "-";
   }
 
+  /* Prints a single gate. */
   void addGate(unsigned line, std::string name) {
     addGates({line}, {name});
   }
 
+  /* Every item in the vector is a pair of:
+   *  1. a list of indices,
+   *  2. a string to put at those indices.
+   * All the gates will be connected by a vertical line. */
   void addGates(std::vector<
                   std::pair<std::vector<unsigned>, std::string>
                 > pairs) {
@@ -28,6 +35,7 @@ public:
     addGates(qubits, names);
   }
 
+  /* Prints a gate spanning all lines of output. */
   void addBarrierGate(std::string name) {
     alignAll();
     for(auto& line : lines)
@@ -52,20 +60,29 @@ public:
     return os;
   }
 
+  /* Normally operator<< would accept a const CircuitPrinter&, but we're
+   * modifying it using alignAll(). The other common situation is printing a
+   * temporary variable, which would not be accepted by the above function.
+   * This trivial wrapper gives the temporary a name (p) and thus converts the
+   * rvalue reference to an lvalue reference. */
   friend std::ostream& operator<< (std::ostream& os, CircuitPrinter&& p) {
-    CircuitPrinter p_{p};
-    return os << p_;
+    return os << p;
   }
 
 private:
 
   void addGates(std::vector<unsigned> qubits, std::vector<std::string> names) {
+    /* Find the longest gate name to be printed */
     unsigned maxLen = 0;
     for(auto& name : names)
       if(length_utf8(name) > maxLen)
         maxLen = length_utf8(name);
+
+    /* Find the total span of the gates being printed */
     auto minmax = std::minmax_element(qubits.begin(), qubits.end());
     align(*minmax.first, *minmax.second);
+
+    /* Print the gate names or connecting | on even (qubit) lines */
     for(unsigned i = *minmax.first; i <= *minmax.second; i++) {
       auto it = std::find(qubits.begin(), qubits.end(), i);
       unsigned pos = it - qubits.begin();
@@ -74,31 +91,39 @@ private:
       else
         lines[2*i] = lines[2*i] + pad("|", maxLen, '-') + "-";
     }
+
+    /* Print the connecting | on odd (padding) lines */
     for(unsigned i = 2*(*minmax.first) + 1; i < 2*(*minmax.second); i += 2)
       lines[i] = lines[i] + pad("|", maxLen, ' ') + ' ';
   }
 
-  void align(unsigned lineFrom, unsigned lineTo, char c = '-') {
+  void align(unsigned lineFrom, unsigned lineTo) {
+    /* Find the longest line wihnin the span so far */
     unsigned max = 0;
     for(unsigned i = 2*lineFrom; i <= 2*lineTo; i++) {
       if(length_utf8(lines[i]) > max)
         max = length_utf8(lines[i]);
     }
+
+    /* Pad the lines with a dash (even: qubit) or a space (odd: padding) */
     for(unsigned i = 2*lineFrom; i <= 2*lineTo; i++)
       if(length_utf8(lines[i]) < max)
-        lines[i] += std::string(max - length_utf8(lines[i]), i&1 ? ' ' : c);
+        lines[i] += std::string(max - length_utf8(lines[i]), i&1 ? ' ' : '-');
   }
 
-  void alignAll(char c = '-') {
-    align(0, nBit - 1, c);
+  void alignAll() {
+    align(0, nBit - 1);
   }
 
+  /* Symetrically pad a string to a given length using a specified character */
   std::string pad(std::string s, unsigned len, char padding) {
     unsigned padLeft = (len - length_utf8(s)) / 2,
              padRight = len - length_utf8(s) - padLeft;
     return std::string(padLeft, padding) + s + std::string(padRight, padding);
   }
 
+  /* This allows names like Φ for quantum gates without distorting
+   * the output. */
   size_t length_utf8(std::string s) {
     size_t len = 0;
     for(char c : s)
