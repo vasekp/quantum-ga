d618d46167879bc0d45949cde09bf216082b6d51
Vašek Potoček <vasek.potocek@post.cz>
2016-11-07 13:47:55 +0000

    CircuitPrinter: reduce number of calls to utf8_length()

--- include/QGA_bits/CircuitPrinter.hpp
+++ include/QGA_bits/CircuitPrinter.hpp
@@ -4,13 +4,63 @@ namespace internal {
 
 class CircuitPrinter {
 
+  /* This allows names like Φ for quantum gates without distorting
+   * the output. */
+  class UTF8String {
+
+  public:
+
+    UTF8String(): s(), len(0) { }
+
+    UTF8String(const std::string& s_): s(s_), len(utf8_length(s)) { }
+
+    UTF8String(char c, size_t len_ = 1): s(len_, c), len(len_) { }
+
+    size_t length() const { return len; }
+
+    operator std::string() const { return s; }
+
+    void operator+= (const UTF8String& other) {
+      s += other.s;
+      len += other.len;
+    }
+
+    friend std::ostream& operator<< (std::ostream& os, const UTF8String& utf) {
+      return os << utf.s;
+    }
+
+    /* Symetrically pad a string to a given length using a specified character */
+    UTF8String pad(size_t target, char padding) const& {
+      size_t padLeft = (target - len) / 2,
+             padRight = target - len - padLeft;
+      UTF8String ret{padding, padLeft};
+      ret += *this;
+      ret += {padding, padRight};
+      return ret;
+    }
+
+  private:
+
+    size_t utf8_length(const std::string& s) {
+      size_t ret = 0;
+      for(char c : s)
+        if((c & 0xC0) != 0x80)
+          ret++;
+      return ret;
+    }
+
+    std::string s;
+    size_t len;
+
+  }; // inner class UTF8String
+
 public:
 
-  CircuitPrinter(unsigned nBit_): nBit(nBit_), lines(2*(nBit - 1)) {
+  CircuitPrinter(unsigned nBit_): nBit(nBit_), lines(2*nBit - 1) {
     // lines[0], lines[2], ..., lines[2*(nBit - 1)]: qubits
     // lines[1], lines[3], ..., lines[2*nBit - 3]: padding space
     for(unsigned i = 0; i < nBit; i++)
-      lines[2*i] = "-";
+      lines[2*i] = '-';
   }
 
   /* Prints a single gate. */
@@ -26,7 +76,7 @@ public:
                   std::pair<std::vector<unsigned>, std::string>
                 > pairs) {
     std::vector<unsigned> qubits{};
-    std::vector<std::string> names{};
+    std::vector<UTF8String> names{};
     for(auto& pair : pairs)
       for(unsigned qubit : pair.first) {
         qubits.push_back(qubit);
@@ -37,15 +87,7 @@ public:
 
   /* Prints a gate spanning all lines of output. */
   void addBarrierGate(std::string name) {
-    alignAll();
-    for(auto& line : lines)
-      line += '[';
-    lines[nBit - 1] += name;
-    alignAll(' ');
-    for(auto& line : lines)
-      line += ']';
-    for(unsigned i = 0; i < nBit; i++)
-      lines[2*i] += '-';
+    addBroadGate(0, nBit - 1, {name});
   }
 
   template<class Gate>
@@ -71,12 +113,12 @@ public:
 
 private:
 
-  void addGates(std::vector<unsigned> qubits, std::vector<std::string> names) {
+  void addGates(std::vector<unsigned> qubits, std::vector<UTF8String> names) {
     /* Find the longest gate name to be printed */
     unsigned maxLen = 0;
     for(auto& name : names)
-      if(length_utf8(name) > maxLen)
-        maxLen = length_utf8(name);
+      if(name.length() > maxLen)
+        maxLen = name.length();
 
     /* Find the total span of the gates being printed */
     auto minmax = std::minmax_element(qubits.begin(), qubits.end());
@@ -86,54 +128,52 @@ private:
     for(unsigned i = *minmax.first; i <= *minmax.second; i++) {
       auto it = std::find(qubits.begin(), qubits.end(), i);
       unsigned pos = it - qubits.begin();
-      if(it != qubits.end())
-        lines[2*i] = lines[2*i] + pad(names[pos], maxLen, '-') + "-";
-      else
-        lines[2*i] = lines[2*i] + pad("|", maxLen, '-') + "-";
+      lines[2*i] += (it != qubits.end()
+          ? names[pos]      // found
+          : UTF8String('|') // not found
+        ).pad(maxLen, '-');
+      lines[2*i] += '-';
     }
 
     /* Print the connecting | on odd (padding) lines */
-    for(unsigned i = 2*(*minmax.first) + 1; i < 2*(*minmax.second); i += 2)
-      lines[i] = lines[i] + pad("|", maxLen, ' ') + ' ';
+    for(unsigned i = 2*(*minmax.first) + 1; i < 2*(*minmax.second); i += 2) {
+      lines[i] += UTF8String('|').pad(maxLen, ' ');
+      lines[i] += ' ';
+    }
   }
 
-  void align(unsigned lineFrom, unsigned lineTo) {
+  void addBroadGate(unsigned lineFrom, unsigned lineTo, UTF8String name) {
+    alignAll();
+    for(unsigned i = 2*lineFrom; i <= 2*lineTo; i++)
+      lines[i] += '[';
+    lines[lineFrom + lineTo] += name; // arithmetic average of 2*lineX
+    alignAll(' ');
+    for(unsigned i = 2*lineFrom; i <= 2*lineTo; i++)
+      lines[i] += ']';
+    for(unsigned i = 2*lineFrom; i <= 2*lineTo; i += 2)
+      lines[i] += '-';
+  }
+
+  void align(unsigned lineFrom, unsigned lineTo, char c = '-') {
     /* Find the longest line wihnin the span so far */
     unsigned max = 0;
     for(unsigned i = 2*lineFrom; i <= 2*lineTo; i++) {
-      if(length_utf8(lines[i]) > max)
-        max = length_utf8(lines[i]);
+      if(lines[i].length() > max)
+        max = lines[i].length();
     }
 
     /* Pad the lines with a dash (even: qubit) or a space (odd: padding) */
     for(unsigned i = 2*lineFrom; i <= 2*lineTo; i++)
-      if(length_utf8(lines[i]) < max)
-        lines[i] += std::string(max - length_utf8(lines[i]), i&1 ? ' ' : '-');
+      if(lines[i].length() < max)
+        lines[i] += {i&1 ? ' ' : c, max - lines[i].length()};
   }
 
-  void alignAll() {
-    align(0, nBit - 1);
-  }
-
-  /* Symetrically pad a string to a given length using a specified character */
-  std::string pad(std::string s, unsigned len, char padding) {
-    unsigned padLeft = (len - length_utf8(s)) / 2,
-             padRight = len - length_utf8(s) - padLeft;
-    return std::string(padLeft, padding) + s + std::string(padRight, padding);
-  }
-
-  /* This allows names like Φ for quantum gates without distorting
-   * the output. */
-  size_t length_utf8(std::string s) {
-    size_t len = 0;
-    for(char c : s)
-      if((c & 0xC0) != 0x80)
-        len++;
-    return len;
+  void alignAll(char c = '-') {
+    align(0, nBit - 1, c);
   }
 
   unsigned nBit;
-  std::vector<std::string> lines;
+  std::vector<UTF8String> lines;
 
 }; // class CircuitPrinter
 
