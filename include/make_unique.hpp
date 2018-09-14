f7f2e821f66fce4cf65131c32d458d60cab6c711
Vašek Potoček <vasek.potocek@post.cz>
2018-09-14 11:29:54 +0200

    Replaced namespace { functions } by inline functions

--- include/make_unique.hpp
+++ include/make_unique.hpp
@@ -1,14 +1,10 @@
 #ifndef MAKE_UNIQUE_HPP
 #define MAKE_UNIQUE_HPP
 
-namespace {
-
 /* Not defined by the standard until C++14 */
 template<class T, typename... Args>
-static std::unique_ptr<T> make_unique(Args... args) {
+inline std::unique_ptr<T> make_unique(Args... args) {
   return std::unique_ptr<T>{new T(std::forward<Args>(args)...)};
 }
-
-} // anonymous namespace
 
 #endif // !defined MAKE_UNIQUE_HPP
